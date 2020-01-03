a885c17f8d9adb53316f8c29dd99f40fa3d09db8
maksdk <havryshchenko@gmail.com>
2020-01-03 18:01:08 +0200

    more examples

--- node/src/application.js
+++ node/src/application.js
@@ -1,6 +1,16 @@
+//@ts-check
+
 import _ from "lodash";
 
 const app = () => {
-    const result = _.flattenDeep([[1], [2], [[3], [4]]]);
+    const result1 = _.flattenDeep([[1], [2], [[3], [4]]]); // [1, 2, 3, 4]
+
+    const object = { 'a': [{ 'b': { 'c': 3 } }] };
+    const result2 = _.get(object, 'a[0].b.c'); // 3
+    const result3 = "result3";
+
+    console.dir(result1);
+    console.log(result2);
+    console.log(result3);
 };
 export default app;

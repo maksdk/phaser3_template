c424ccac688a6099baf43fd70b9f1e30eade3ff0
maksdk <havryshchenko@gmail.com>
2020-01-03 17:16:45 +0200

    cleared

--- node/.compiled/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";var _application = _interopRequireDefault(require("./application.js"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-(0, _application.default)();
-//# sourceMappingURL=index.js.map
